1bb4cd7f44a8f990912259158505dcfedf5ef592
BoRamSon <onbrson1@gmail.com>
2024-11-25 16:37:37 +0900

    feat(controller): [입력] 보너스 번호를 입력 받는다.

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,14 +1,22 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import InputPurchaseModule from '../modules/InputPurchaseModule.js';
 import OutputView from '../view/OutputView.js';
+import InputPurchaseModule from '../modules/InputPurchaseModule.js';
+import InputWinningModule from '../modules/InputWinningModule.js';
+import InputBonusModule from '../modules/InputBonusModule.js';
 
 class LottoController {
   constructor() {
     this.inputPurchaseModule = new InputPurchaseModule();
+    this.inputWinningModule = new InputWinningModule();
+    this.inputBonusModule = new InputBonusModule();
   }
+
   async runLotto() {
     const purchasQuantity = await this.inputPurchaseModule.inputPurchaseAmount();
     const lottoNumber = this.printRandomNumber(purchasQuantity);
+    const winningNumber = await this.inputWinningModule.inputWinningNumber();
+    const bonusNumber = await this.inputBonusModule.inputBonusNumber();
+    console.log(bonusNumber);
   }
 
   printRandomNumber(purchasQuantity) {
@@ -20,6 +28,7 @@ class LottoController {
       OutputView.printLottoNumber(ascendingOrder);
       lottoNumberArrayss = [...lottoNumberArrayss, ascendingOrder];
     }
+    OutputView.printSpace();
     return lottoNumberArrayss;
   }
 }
