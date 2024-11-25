c1f4bf8103caf34d1d1d50a76c5293eca1cce88e
BoRamSon <onbrson1@gmail.com>
2024-11-25 15:30:38 +0900

    feat(contorller): [출력] 발행한 로또 수량 및 번호를 출력한다.

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,11 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import InputPurchaseModule from '../modules/InputPurchaseModule.js';
+import OutputView from '../view/OutputView.js';
 
 class LottoController {
   constructor() {
     this.inputPurchaseModule = new InputPurchaseModule();
   }
   async runLotto() {
-    const input = await this.inputPurchaseModule.inputPurchaseAmount();
+    const purchasQuantity = await this.inputPurchaseModule.inputPurchaseAmount();
+    const lottoNumber = this.printRandomNumber(purchasQuantity);
+  }
+
+  printRandomNumber(purchasQuantity) {
+    OutputView.printPurchaseNumber(purchasQuantity);
+    let lottoNumberArrayss = [];
+    for (let i = 0; i < purchasQuantity; i++) {
+      const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      OutputView.printLottoNumber(lottoNumber);
+      lottoNumberArrayss = [...lottoNumberArrayss, lottoNumber];
+    }
+    return lottoNumberArrayss;
   }
 }
 
