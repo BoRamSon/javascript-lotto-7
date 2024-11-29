2e0ea5cc446e33e6f0edf2a4417491a807dc9604
BoRamSon <onbrson1@gmail.com>
2024-11-29 23:28:50 +0900

    chore(constants): outputMessages 키명칭 카멜케이스로 변경에 따른 print 명칭 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,30 +3,34 @@ import OUTPUT_MESSAGES from '../constants/outputMessages.js';
 
 const OutputView = {
   async printPurchaseNumber(purchasQuantity) {
-    return MissionUtils.Console.print(`${purchasQuantity}${OUTPUT_MESSAGES.PURCHASE_NUMBER}`);
+    return MissionUtils.Console.print(`${purchasQuantity}${OUTPUT_MESSAGES.purchaseNumber}`);
+  },
+
+  async printWinningMessage() {
+    return MissionUtils.Console.print(OUTPUT_MESSAGES.winningStatistics);
   },
 
   async printWinningStatistics(three, four, five, fiveAndBonus, six) {
     MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_THREE}${three}${OUTPUT_MESSAGES.PRINT_QUANTITY}`
+      `${OUTPUT_MESSAGES.MATCHED_THREE}${three}${OUTPUT_MESSAGES.printQuantity}`
     );
     MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_FOUR}${four}${OUTPUT_MESSAGES.PRINT_QUANTITY}`
+      `${OUTPUT_MESSAGES.matchedFour}${four}${OUTPUT_MESSAGES.printQuantity}`
     );
     MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_FIVE}${five}${OUTPUT_MESSAGES.PRINT_QUANTITY}`
+      `${OUTPUT_MESSAGES.matchedFive}${five}${OUTPUT_MESSAGES.printQuantity}`
     );
     MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_FIVE_BONUS}${fiveAndBonus}${OUTPUT_MESSAGES.PRINT_QUANTITY}`
+      `${OUTPUT_MESSAGES.matchedFiveBonus}${fiveAndBonus}${OUTPUT_MESSAGES.printQuantity}`
     );
     MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_SIX}${six}${OUTPUT_MESSAGES.PRINT_QUANTITY}`
+      `${OUTPUT_MESSAGES.matchedSix}${six}${OUTPUT_MESSAGES.printQuantity}`
     );
   },
 
   async printTotalReturn(percentage) {
     return MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.TOTAL_RETURN}${percentage}${OUTPUT_MESSAGES.TOTAL_RETURN_PERCENTAGE}`
+      `${OUTPUT_MESSAGES.totalReturn}${percentage}${OUTPUT_MESSAGES.totalReturnPercentage}`
     );
   },
 
