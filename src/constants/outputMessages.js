6b3b3f9c44116314b6ebd4d78588ace4d5be39bc
BoRamSon <onbrson1@gmail.com>
2024-11-29 23:28:27 +0900

    chore(constants): outputMessages 키명칭 카멜케이스로 변경

--- src/constants/outputMessages.js
+++ src/constants/outputMessages.js
@@ -1,14 +1,14 @@
 const OUTPUT_MESSAGES = {
-  PURCHASE_NUMBER: '개를 구매했습니다.',
-  WINNING_STATISTICS: '당첨통계\n--- ',
-  MATCHED_THREE: '3개 일치 (5,000원) - ',
-  MATCHED_FOUR: '4개 일치 (50,000원) - ',
-  MATCHED_FIVE: '5개 일치 (1,500,000원) - ',
-  MATCHED_FIVE_BONUS: '5개 일치 (30,000,000원) - ',
-  MATCHED_SIX: '6개 일치 (2,000,000,000원) - ',
-  PRINT_QUANTITY: '개',
-  TOTAL_RETURN: '총 수익률은 ',
-  TOTAL_RETURN_PERCENTAGE: '% 입니다.',
+  purchaseNumber: '개를 구매했습니다.',
+  winningStatistics: '당첨통계\n--- ',
+  matchedThree: '3개 일치 (5,000원) - ',
+  matchedFour: '4개 일치 (50,000원) - ',
+  matchedFive: '5개 일치 (1,500,000원) - ',
+  matchedFiveBonus: '5개 일치 (30,000,000원) - ',
+  matchedSix: '6개 일치 (2,000,000,000원) - ',
+  printQuantity: '개',
+  totalReturn: '총 수익률은 ',
+  totalReturnPercentage: '% 입니다.',
 };
 
 export default OUTPUT_MESSAGES;
