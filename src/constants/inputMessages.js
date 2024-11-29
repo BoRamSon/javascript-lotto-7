ad80c86dd8ff7fd4357881c01aac7bb6d6b2596a
BoRamSon <onbrson1@gmail.com>
2024-11-29 23:30:18 +0900

    chore(constants): inputMessages 키명칭 카멜케이스로 변경

--- src/constants/inputMessages.js
+++ src/constants/inputMessages.js
@@ -1,7 +1,7 @@
 const INPUT_MESSAGES = {
-  WHAT_PURCHASE_AMOUNT: '구매금액을 입력해주세요. \n => ',
-  WINNING_NUMBER: '당첨번호를 입력해주세요. \n => ',
-  BONUS_NUMBER: '보너스번호를 입력해주세요. \n => ',
+  whatPurchaseAmount: '구매금액을 입력해주세요. \n => ',
+  winningNumber: '당첨번호를 입력해주세요. \n => ',
+  bonusNumber: '보너스번호를 입력해주세요. \n => ',
 };
 
 export default INPUT_MESSAGES;
