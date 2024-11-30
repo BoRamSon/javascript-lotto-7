2303ec15a06b23a0a135b02da6298170b28ad883
BoRamSon <onbrson1@gmail.com>
2024-11-30 23:48:24 +0900

    chore: matchedFiveBonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ', 에서 보너스 볼 일치 메세지 추가

--- src/constants/outputMessages.js
+++ src/constants/outputMessages.js
@@ -4,7 +4,7 @@ const OUTPUT_MESSAGES = {
   matchedThree: '3개 일치 (5,000원) - ',
   matchedFour: '4개 일치 (50,000원) - ',
   matchedFive: '5개 일치 (1,500,000원) - ',
-  matchedFiveBonus: '5개 일치 (30,000,000원) - ',
+  matchedFiveBonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   matchedSix: '6개 일치 (2,000,000,000원) - ',
   printQuantity: '개',
   totalReturn: '총 수익률은 ',
