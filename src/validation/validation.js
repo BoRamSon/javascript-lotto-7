e247daab6bec4cc944205750fa2f9e804776aeb8
BoRamSon <onbrson1@gmail.com>
2024-11-30 17:21:18 +0900

    feat(validation) : throw new Error를 createThrowError() 함수로 변경

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,41 +1,42 @@
 import { ERROR_MESSAGES } from '../constants/errorMessages.js';
+import createThrowError from '../utils/createError.js';
 
 class validation {
   empty(value) {
-    if (value === '') throw new Error(ERROR_MESSAGES.EMPTY_VALUE);
+    if (value === '') createThrowError(ERROR_MESSAGES.emptyValue);
     return true;
   }
 
   regularExpression(value, regExp) {
     const regExpTest = regExp.test(value);
-    if (!regExpTest) throw new Error(ERROR_MESSAGES.REGEXP_TEST);
+    if (!regExpTest) createThrowError(ERROR_MESSAGES.regexpTest);
     return true;
   }
 
   startedComma(value) {
     const regExp = /^,/;
     const regExpTest = regExp.test(value);
-    if (regExpTest) throw new Error(ERROR_MESSAGES.START_COMMA);
+    if (regExpTest) createThrowError(ERROR_MESSAGES.startComma);
     return true;
   }
 
   endedComma(value) {
     const regExp = /,$/;
     const regExpTest = regExp.test(value);
-    if (regExpTest) throw new Error(ERROR_MESSAGES.END_COMMA);
+    if (regExpTest) createThrowError(ERROR_MESSAGES.endComma);
     return true;
   }
 
   nagativeNumber(value) {
     const changeTypeNumber = Number(value);
-    if (0 > changeTypeNumber) throw new Error(ERROR_MESSAGES.NAGATIVE_NUMBER);
+    if (0 > changeTypeNumber) createThrowError(ERROR_MESSAGES.negativeNumber);
     return true;
   }
 
   limitDigits(value) {
     const regExp = /\d{4,6}$/;
     const regExpTest = regExp.test(value);
-    if (!regExpTest) throw new Error(ERROR_MESSAGES.LIMIT_DIGITS);
+    if (!regExpTest) createThrowError(ERROR_MESSAGES.limitDigits);
     return true;
   }
 
@@ -43,8 +44,8 @@ class validation {
     const changeTypeNumber = Number(value);
     const target = changeTypeNumber / 1000;
     const isInteger = Number.isInteger(target);
-    if (!isInteger) throw new Error(ERROR_MESSAGES.THOUSAND_UNIT);
-    if (0 < target && target > 101) throw new Error(ERROR_MESSAGES.THOUSAND_UNIT);
+    if (!isInteger) createThrowError(ERROR_MESSAGES.thiusandUnit);
+    if (0 < target && target > 101) createThrowError(ERROR_MESSAGES.THOUSAND_UNIT);
     return true;
   }
 }
