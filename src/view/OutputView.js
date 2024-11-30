c42cb60b300935b7af1d94e0bb78368c2e321d8d
BoRamSon <onbrson1@gmail.com>
2024-11-30 23:23:32 +0900

    chore: return 제거

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,47 +3,35 @@ import OUTPUT_MESSAGES from '../constants/outputMessages.js';
 
 const OutputView = {
   async printPurchaseNumber(purchasQuantity) {
-    return MissionUtils.Console.print(`${purchasQuantity}${OUTPUT_MESSAGES.purchaseNumber}`);
+    MissionUtils.Console.print(`${purchasQuantity}${OUTPUT_MESSAGES.purchaseNumber}`);
   },
 
   async printWinningMessage() {
-    return MissionUtils.Console.print(OUTPUT_MESSAGES.winningStatistics);
+    MissionUtils.Console.print(OUTPUT_MESSAGES.winningStatistics);
   },
 
   async printWinningStatistics(three, four, five, fiveAndBonus, six) {
-    MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.MATCHED_THREE}${three}${OUTPUT_MESSAGES.printQuantity}`
-    );
-    MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.matchedFour}${four}${OUTPUT_MESSAGES.printQuantity}`
-    );
-    MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.matchedFive}${five}${OUTPUT_MESSAGES.printQuantity}`
-    );
-    MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.matchedFiveBonus}${fiveAndBonus}${OUTPUT_MESSAGES.printQuantity}`
-    );
-    MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.matchedSix}${six}${OUTPUT_MESSAGES.printQuantity}`
-    );
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.MATCHED_THREE}${three}${OUTPUT_MESSAGES.printQuantity}`);
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.matchedFour}${four}${OUTPUT_MESSAGES.printQuantity}`);
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.matchedFive}${five}${OUTPUT_MESSAGES.printQuantity}`);
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.matchedFiveBonus}${fiveAndBonus}${OUTPUT_MESSAGES.printQuantity}`);
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.matchedSix}${six}${OUTPUT_MESSAGES.printQuantity}`);
   },
 
   async printTotalReturn(percentage) {
-    return MissionUtils.Console.print(
-      `${OUTPUT_MESSAGES.totalReturn}${percentage}${OUTPUT_MESSAGES.totalReturnPercentage}`
-    );
+    MissionUtils.Console.print(`${OUTPUT_MESSAGES.totalReturn}${percentage}${OUTPUT_MESSAGES.totalReturnPercentage}`);
   },
 
   async printLottoNumber(value) {
-    return MissionUtils.Console.print(value);
+    MissionUtils.Console.print(value);
   },
 
   async printError(error) {
-    return MissionUtils.Console.print(error);
+    MissionUtils.Console.print(error);
   },
 
   async printSpace() {
-    return MissionUtils.Console.print('');
+    MissionUtils.Console.print('');
   },
 };
 
